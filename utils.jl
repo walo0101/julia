e645a07d2b962ba9851b173c90797f8c2aabd1f0
DuyenNguyen <88462976+walo0101@users.noreply.github.com>
2023-11-09 15:52:48 +0100

    Update utils.jl

--- utils.jl
+++ utils.jl
@@ -1,2 +1,9 @@
 # identity matrix
 I = diagm(0=>ones(3))
+
+#
+Pkg.add("PyCall")
+using PyCall
+run(`$(PyCall.python) -m pip install pydicom`)
+pydicom = pyimport("pydicom")
+img = pydicom.read_file("HN_CHUM_004_0051.dcm")
